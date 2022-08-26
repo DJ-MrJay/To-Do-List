b428ce3d7dc4e256cf2d0000b0ded0bf3e6277e8
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-08-26 15:05:43 +0300

    Fix linters errors

--- src/modules/closeButton.test.js
+++ src/modules/closeButton.test.js
@@ -1,12 +1,12 @@
-import { closeButton } from './closeButton';
+import { closeButton } from './closeButton.js';
 
 jest.mock('./variables.js');
 
 describe('Remove a list item', () => {
   it('should expose a function', () => {
-		expect(closeButton).toBeDefined();
-	});
-  
+    expect(closeButton).toBeDefined();
+  });
+
   it('Close button should remove a list item', () => {
     // const retValue = closeButton(index);
     expect(false).toBeTruthy();
--- src/modules/insertNewItem.test.js
+++ src/modules/insertNewItem.test.js
@@ -1,12 +1,12 @@
-import { insertNewItem } from './insertNewItem';
+import { insertNewItem } from './insertNewItem.js';
 
 jest.mock('./variables.js');
 
 describe('Add a new list item', () => {
   it('should expose a function', () => {
-		expect(insertNewItem).toBeDefined();
-	});
-  
+    expect(insertNewItem).toBeDefined();
+  });
+
   it('Added item should be displayed', () => {
     // const retValue = insertNewItem(text);
     expect(false).toBeTruthy();
